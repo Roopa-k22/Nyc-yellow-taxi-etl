608817444a5c215a334954fa0aabd1346da30f12
Roopa Krishna <roopa222k@gmail.com>
2025-10-25 13:23:51 -0400

    cleared python path

--- etl/setup.py
+++ etl/setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Nyc_yellow_taxi_etl_pipeline',
-    version='1.0',
-    packages=find_packages(),
+    name='Nyc_yellow_taxi_etl',
+    version='1.0.0',
+    packages=find_packages(),  # will find the 'etl' package automatically
     install_requires=[
         'apache-beam[gcp]==2.58.0',
         'google-cloud-bigquery==3.10.0',
-        'google-cloud-storage==2.14.0'
+        'google-cloud-storage>=2.16.0,<3',  # updated to avoid Beam conflicts
+        'pytest==8.3.0'
     ],
+    python_requires='>=3.9',
 )
